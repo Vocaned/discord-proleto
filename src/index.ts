f0d48cb5dc36f6823b38bc1684e88b4adc625e8c
Vocaned <10358674+Vocaned@users.noreply.github.com>
2023-08-15 15:27:18 +0300

    Handle timezones properly, keep retrying if errors

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,16 @@ import { COUNTRIES } from './countries';
 interface Env {
     DISCORD_TOKEN: string;
     WORSHIP_CHANNEL_ID: string;
+    DATA: KVNamespace;
 }
 
-const random = (seed: number): number => {
-    var x = Math.sin(seed) * 10000;
+let random = (seed: number): number => {
+    let x = Math.sin(seed) * 10000;
     return x - Math.floor(x);
 }
 
-const worship = async (env: Env): Promise<string> => {
-    const d = new Date();
+let worship = async (env: Env): Promise<string> => {
+    let d = new Date();
     let seed = parseInt(
       `${d.getFullYear()}${d.getMonth() + 1}${d.getDate()}`
     );
@@ -30,17 +31,25 @@ const worship = async (env: Env): Promise<string> => {
     });
 
     let res = await req.text()
-
-    if (req.status !== 200) throw res;
+    if (req.status !== 200) throw new Error(res);
     return res;
 }
 
 export default {
     async scheduled(event: ScheduledEvent, env: Env, ctx: ExecutionContext) {
-        return Response.json(JSON.parse(await worship(env)));
+        // Get the date in Helsinki as a string. sv-SE is used as locale for YYYY-MM-SS format.
+        let date = new Date().toLocaleString('sv-SE', {timeZone: 'Europe/Helsinki'});
+        let lastDate = await env.DATA.get('last_worship');
+
+        if (!lastDate || date.split(' ')[0] !== lastDate.split(' ')[0]) {
+            await worship(env);
+            await env.DATA.put('last_worship', date);
+        }
+
+        return Response.json({}, {status: 200});
     },
     async fetch(request: Request, env: Env, ctx: ExecutionContext) {
-        const { pathname } = new URL(request.url);
+        let { pathname } = new URL(request.url);
 
         if (pathname === '/proleto/worship') return Response.json(JSON.parse(await worship(env)));
 
