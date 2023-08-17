7842ed6e97b5ddf5c168506976867d418259209c
Fam0r <fam0r@mailbox.org>
2023-08-17 12:33:25 +0300

    Improve differ more

--- src/differ.ts
+++ src/differ.ts
@@ -18,8 +18,8 @@ export let diff = async (differ: Differ, old: string): Promise<string> => {
     if (differ.type === 'json') {
         if (!old) old = '{}';
         // Format JSON
-        oldContent = JSON.stringify(JSON.parse(old), null, 2).split('\n');
-        newContent = JSON.stringify(JSON.parse(content), null, 2).split('\n');
+        oldContent = JSON.stringify(JSON.parse(old), null, 2).split('\n').map(l => l.trim());
+        newContent = JSON.stringify(JSON.parse(content), null, 2).split('\n').map(l => l.trim());
     } else if (differ.type === 'text') {
         oldContent = old.split('\n');
         newContent = content.split('\n');
@@ -27,46 +27,26 @@ export let diff = async (differ: Differ, old: string): Promise<string> => {
         throw new Error(`Unknown parser ${differ.type}`);
     }
 
-    let added: string[] = [];
-    let removed: string[] = [];
-
-    let maxLength = Math.max(oldContent.length, newContent.length);
-    for (let i = 0; i < maxLength; i++) {
-        let oldLine = (oldContent[i] ?? '').trim();
-        let newLine = (newContent[i] ?? '').trim();
-
-        if (oldLine === newLine) continue;
-
-        if (!oldContent.includes(newLine)) added.push(newLine);
-        else if (!newContent.includes(oldLine)) removed.push(oldLine);
-    }
+    let added = newContent.filter(l => !oldContent.includes(l));
+    let removed = newContent.filter(l => !newContent.includes(l));
 
     let output = '';
-    if (added.length > 0) {
-        output += '## Added\n```diff\n';
-        for (let l of added) {
-            output += `+ ${l}\n`;
-        }
-        output += '```\n';
+    for (let l of added) {
+        output += `+ ${l}\n`;
     }
-    if (removed.length > 0) {
-        output += '## Removed\n```diff\n';
-        for (let l of removed) {
-            output += `- ${l}\n`;
-        }
-        output += '```\n';
+    for (let l of removed) {
+        output += `- ${l}\n`;
     }
 
     if (output) {
         output = output.substring(0, 4090)
-        if (!output.endsWith('```')) output += '```';
 
         await fetch(differ.webhook, {
             method: 'POST',
             body: JSON.stringify({
                 embeds: [{
                     title: `${differ.id}`,
-                    description: output,
+                    description: '```diff\n' + output + '```',
                     url: differ.fetch_url
                 }]
             }),
