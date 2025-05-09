8be3fff31149c8fc8c29160c7ec59c4fa625e616
Vocane <vocane@mailbox.org>
2025-05-09 21:15:42 +0300

    awful, horrible code.

---
+++ src/daily.ts
@@ -0,0 +1,158 @@
+import { SUBDIVISIONS } from './data/subdivisions';
+import { Env } from '.';
+import * as cheerio from 'cheerio';
+import { AnyNode } from 'domhandler';
+
+const random = (seed: number): number => {
+    let x = Math.sin(seed) * 10000;
+    return x - Math.floor(x);
+}
+
+let comment = '';
+
+function processNode(dom: cheerio.CheerioAPI, node: cheerio.Cheerio<AnyNode>, depth: number = 0, stack: Array<number> = [], inList: boolean = false) {
+    let output = '';
+
+    dom(node).contents().each((_: any, el: any) => {
+        const $el = dom(el);
+
+        if (dom($el.children().first()).attr("typeof") === 'mw:File') {
+            // drop comment parts of the description
+            comment = $el.text();
+        } else if (el.type === 'text') {
+            output += $el.text();
+        } else if (el.tagName === 'ol') {
+            stack.push(1); // start new list
+            output += processNode(dom, el, depth + 1, stack, false);
+            stack.pop();
+        } else if (el.tagName === 'li') {
+            const number = stack[stack.length - 1]++;
+            let d = Math.max(0, depth - 1);
+            output += `${'  '.repeat(d)}${number}. ${processNode(dom, el, depth + 1, stack, true).trim()}`;
+        } else if (el.tagName === 'a') {
+            const href = $el.attr('href') || '#';
+            const text = $el.text().trim();
+            output += inList ? text : `[${text}](https://en.wiktionary.org${href})`;
+        } else if (el.tagName === 'i') {
+            output += `*${processNode(dom, el, depth, stack, inList).trim()}*`;
+        } else if (el.tagName === 'b') {
+            output += `**${processNode(dom, el, depth, stack, inList).trim()}**`;
+        } else {
+            output += processNode(dom, el, depth, stack, inList).trim();
+        }
+    });
+
+    return output;
+}
+
+interface wikiparse {
+    error: any,
+    parse: {
+        text: {
+            '*': string;
+        }
+    }
+}
+
+export const words = async (env: Env): Promise<string> => {
+    // Get the date in Helsinki as a string in format "Mm D, YYYY"
+    let date = new Date().toLocaleString('en-US', { year: 'numeric', month: 'long', day: 'numeric', timeZone: 'Europe/Helsinki' });
+    let [day, year] = date.split(', ');
+
+    let components: any = [{
+        type: 10, // text
+        content: `### Words of the day\n-# ${day} ${year}`
+    }];
+
+    // WOTD
+    let wotdhtml = await fetch(`https://en.wiktionary.org/w/api.php?action=parse&format=json&prop=text&page=Wiktionary:Word of the day/${year}/${day}`);
+    let wotddata = await wotdhtml.json() as wikiparse;
+    if (typeof wotddata.error === 'undefined') {
+        let wotd = cheerio.load(wotddata.parse.text['*']);
+        let wotdtitle = processNode(wotd, wotd(wotd("#WOTD-rss-title").parentsUntil('tr').last())).trim();
+        let wotddesc = processNode(wotd, wotd("#WOTD-rss-description")).trim();
+
+        let wotdcomment = '';
+        if (comment) {
+            wotdcomment = `\n-# - ${comment}`;
+            comment = '';
+        }
+
+        components.push({
+            type: 14 // separator
+        }, {
+            type: 10,
+            content: `-# English\n${wotdtitle}\n${wotddesc}${wotdcomment}`
+        });
+    }
+
+    // FWOTD
+    let fwotdhtml = await fetch(`https://en.wiktionary.org/w/api.php?action=parse&format=json&prop=text&page=Wiktionary:Foreign Word of the Day/${year}/${day}`);
+    let fwotddata = await fwotdhtml.json() as wikiparse;
+    if (typeof fwotddata.error === 'undefined') {
+        let fwotd = cheerio.load(fwotddata.parse.text['*']);
+        let fwotdtitle = processNode(fwotd, fwotd(".headword-line")).trim();
+        let fwotddesc = processNode(fwotd, fwotd("#FWOTD-rss-description")).trim();
+        let fwotdlang = fwotd("#FWOTD-rss-language").text().trim();
+
+        let fwotdcomment = '';
+        if (comment) {
+            fwotdcomment = `\n-# - ${comment}`;
+            comment = '';
+        }
+        components.push({
+            type: 14 // separator
+        }, {
+            type: 10,
+            content: `-# ${fwotdlang}\n${fwotdtitle}\n${fwotddesc}${fwotdcomment}`
+        });
+    }
+
+    let req = await fetch(`https://discord.com/api/v10/channels/${env.WORSHIP_CHANNEL_ID}/messages`, {
+        method: 'POST',
+        body: JSON.stringify({
+            flags: 1 << 15, // Components V2
+            components: [{
+                type: 17, // container
+                components: components
+            }]
+        }),
+        headers: {
+            Authorization: 'Bot ' + env.DISCORD_TOKEN,
+            'Content-Type': 'application/json'
+        }
+    });
+
+    let res = await req.text();
+    if (req.status !== 200) throw new Error(res);
+    return res;
+}
+
+export const worship = async (env: Env): Promise<string> => {
+    // Get the date in Helsinki as a string. sv-SE is used as locale for YYYY-MM-SS format.
+    let date = new Date().toLocaleString('sv-SE', { year: 'numeric', month: 'numeric', day: 'numeric', timeZone: 'Europe/Helsinki' });
+    // Remove leading zeroes to get YYYYMD for legacy purposes
+    let seed = parseInt(
+        date.split('-').map(component => parseInt(component).toString()).join('')
+    );
+
+    let subdivision = SUBDIVISIONS[Math.trunc(random(seed) * SUBDIVISIONS.length)];
+    let country = subdivision.parent.toLowerCase().split('-')[0];
+
+    let req = await fetch(`https://discord.com/api/v10/channels/${env.WORSHIP_CHANNEL_ID}`, {
+        method: 'PATCH',
+        body: JSON.stringify({
+            name: `${subdivision.name}-worshipping`,
+            topic: `:flag_${country}:`.repeat(3)
+        }),
+        headers: {
+            Authorization: 'Bot ' + env.DISCORD_TOKEN,
+            'Content-Type': 'application/json'
+        }
+    });
+
+    let res = await req.text();
+    if (req.status !== 200) throw new Error(res);
+    return res;
+}
+
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import { worship } from './worship';
+import { worship, words } from './daily';
 import { Differ, diff } from './differ';
 
-interface Env {
+export interface Env {
     DISCORD_TOKEN: string;
     WORSHIP_CHANNEL_ID: string;
     PASTE_API_KEY: string;
@@ -14,16 +14,17 @@ interface IDStub {
     id: string;
 }
 
-export default {
-    async scheduled(event: ScheduledEvent, env: Env, ctx: ExecutionContext) {
-        // Get the date in Helsinki as a string. sv-SE is used as locale for YYYY-MM-SS format.
-        let date = new Date().toLocaleString('sv-SE', {year: 'numeric', month: 'numeric', day: 'numeric', timeZone: 'Europe/Helsinki'});
-        let lastDate = await env.DATA.get('last_worship');
+const SCHEDULE: { [hour: string]: (env: Env) => Promise<string> } = {
+    '00': worship,
+    '12': words,
+    //'18': bible
+}
 
-        if (!lastDate || date !== lastDate) {
-            await worship(date, env.WORSHIP_CHANNEL_ID, env.DISCORD_TOKEN);
-            await env.DATA.put('last_worship', date);
-        }
+export default {
+    async scheduled(_event: ScheduledEvent, env: Env, _ctx: ExecutionContext) {
+        // TODO: Handle retrying the next hour in case of failure (e.g. discord downtime)
+        let hour = new Date().toLocaleString('sv-SE', { hour: '2-digit', hour12: false, timeZone: 'Europe/Helsinki' });
+        if (SCHEDULE[hour]) await SCHEDULE[hour](env)
 
         let differs = await env.DATA.get<Differ[]>('differs', 'json');
         if (differs) {
@@ -34,14 +35,16 @@ export default {
             }
         }
 
-        return Response.json({}, {status: 200});
+        return Response.json({}, { status: 200 });
     },
-    async fetch(request: Request, env: Env, ctx: ExecutionContext) {
+    async fetch(request: Request, env: Env, _ctx: ExecutionContext) {
         let { pathname } = new URL(request.url);
 
         if (pathname === '/proleto/worship') {
-            let date = new Date().toLocaleString('sv-SE', {year: 'numeric', month: 'numeric', day: 'numeric', timeZone: 'Europe/Helsinki'});
-            return Response.json(JSON.parse(await worship(date, env.WORSHIP_CHANNEL_ID, env.DISCORD_TOKEN)));
+            return Response.json(JSON.parse(await worship(env)));
+        }
+        if (pathname === '/proleto/words') {
+            return Response.json(JSON.parse(await words(env)));
         }
         if (pathname === '/proleto/differs' && request.method === 'GET') {
             return Response.json(await env.DATA.get<Differ[]>('differs', 'json'));
@@ -55,11 +58,11 @@ export default {
                     await env.DIFFDATA.put(differ.id, newcontent);
                 }
             }
-            return Response.json({}, {status: 200});
+            return Response.json({}, { status: 200 });
         }
         if (pathname === '/proleto/differs' && request.method === 'PUT') {
             let newDiffer = await request.json<Differ>();
-            if (!newDiffer.id || !newDiffer.type || !newDiffer.webhook || !newDiffer.fetch_url || !newDiffer.fetch_opts) return Response.json({error: 'Invalid differ'}, {status: 400});
+            if (!newDiffer.id || !newDiffer.type || !newDiffer.webhook || !newDiffer.fetch_url || !newDiffer.fetch_opts) return Response.json({ error: 'Invalid differ' }, { status: 400 });
 
             let differs = await env.DATA.get<Differ[]>('differs', 'json');
             if (!differs) differs = [];
@@ -72,13 +75,13 @@ export default {
             let body = await request.json<IDStub>();
             let differs = await env.DATA.get<Differ[]>('differs', 'json');
 
-            if (!body.id || !differs) return Response.json({error: 'Invalid differ ID'}, {status: 400});
+            if (!body.id || !differs) return Response.json({ error: 'Invalid differ ID' }, { status: 400 });
             differs = differs.filter(e => e.id !== body.id);
             await env.DATA.put('differs', JSON.stringify(differs));
             await env.DIFFDATA.delete(body.id);
-            return Response.json({id: body.id});
+            return Response.json({ id: body.id });
         }
 
-        return Response.json({error: "I'm a teapot", code: 418}, {status: 418});
+        return Response.json({ error: "I'm a teapot", code: 418 }, { status: 418 });
     }
 };
